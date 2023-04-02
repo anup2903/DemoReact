8dafa99dc4b842fa0203f5c0bcfb3ae7e8bd51c0
Anup <106077220+anup2903@users.noreply.github.com>
2023-04-02 13:25:30 +0530

    commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,23 +3,39 @@ import Nav from './Nav'
 
 const Login = () => {
   return (
-    <div>
+    <div className='log '>
       <Nav />
 
-      <div className="h-56 w-80 ml-[40rem] my-60 bg-pink-300">
-        <div className="flex space-x-5 pt-14 justify-center">
-          <h1>Username</h1>
-          <input className="bg-yellow-200" type="text" />
+      <div className="mt-32 ml-20">
+        <h1 className="font-semibold text-zinc-400">START FOR FREE</h1>
+        <h1 className="mt-4 font-bold text-4xl text-zinc-50">Create a new account</h1>
+        <h3 className="mt-4 text-zinc-400">Already a member? Log In</h3>
+        <div className="mt-2">
+          <form className='flex flex-col '>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your Name"
+              className=" w-60 rounded-xl p-2 mt-4 bg-zinc-300"
+            />
+
+            <input
+              type="password"
+              name="pass"
+              placeholder="Enter your Password"
+              className=" w-60 rounded-xl p-2 mt-4 bg-zinc-300"
+            />
+            <button className='mt-4 rounded-xl button w-60 justify-center p-2'>Create Account</button>
+          </form>
         </div>
-        <div className="pt-12 flex space-x-5 justify-center">
-          <h1>Password</h1>
-          <input
-            className="bg-purple-300"
-            type="password"
-            name="password"
-            id=""
-          />
+
+        <div className='flex'>
+          <hr className='fill-orange-50 border-solid' />
+          <h1 className='fill-slate-100'>OR</h1>
+          <hr />
         </div>
+
+        
       </div>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,32 @@
 import React,{useState} from 'react'
 import {Link} from "react-router-dom"
+import green from '../images/green.png'
+import moon from '../images/full-moon.png';
 const Nav = () => {
 
     
   return (
     <nav>
-      <ul className="flex justify-between py-3 bg-blue-400">
-        <li className="pl-2 font-mono text-3xl">Demo</li>
+      <ul className="flex justify-between py-3 ">
+      <div className='flex'>
+        <img src={green} className='w-13 h-10' alt="" />
+        <li className="pl-2 font-mono text-3xl text-gray-50">Demo</li>
+      </div>
+        
         <div className="items flex space-x-11 pr-10 pt-1">
           <Link to="/">
             <li className="text-xl">Home</li>
           </Link>
 
-          <Link to="about">
+          <Link to="/about">
             <li className="text-xl ">About</li>
           </Link>
 
-          <Link to="login">
+          <Link to="/login">
             <li className="text-xl cursor-pointer">Login</li>
           </Link>
         </div>
       </ul>
-      
     </nav>
   );
 }
